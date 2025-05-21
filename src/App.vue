fe83ad1dd4efc4bcc9bee924b40d40580bf897f6
cristian camilo lópez giraldo <k-milo3110@hotmail.es>
2025-05-20 21:02:10 -0500

    primer commit de vue personas

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
-  <nav>
+  <div id="nav">
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+    <router-link to="/about">About</router-link> |
+    <router-link to="/comunas">Comunas</router-link> |
+  </div>
   <router-view/>
 </template>
+
 
 <style>
 #app {
